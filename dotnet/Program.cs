488bcd6b62d2b5f30121ff5930cd863ea2388c29
VinnyHC <vinny.mannello@hashicorp.com>
2022-05-18 10:19:41 -0700

    dotnet

---
+++ dotnet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace VaultPOC
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //TODO HC-DevEx-2022: Replace `sampleSecret` with secret fetched from Vault
+            var sampleSecret = "❌ Never Hardcode Secrets";
+
+            Console.WriteLine(sampleSecret);
+        }
+    }
+}
+// [END all]
