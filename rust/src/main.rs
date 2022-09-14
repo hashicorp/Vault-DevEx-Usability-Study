2055b38490f3a9c87bec161d4dc8ccebc89fe3a5
Daniel Huckins <dhuckins@users.noreply.github.com>
2022-09-14 17:28:22 -0400

    add (not working) rust example

---
+++ rust/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+use hashicorp_vault;
+
+fn main() -> Result<(), ()> {
+    let vault_address = env::var("VAULT_ADDR")
+        .expect("VAULT_ADDR must be set");
+    let vault_token = env::var("VAULT_TOKEN")
+        .expect("VAULT_TOKEN must be set");
+
+    let vault_client = hashicorp_vault::Client::new(vault_address, vault_token).unwrap();
+
+    let secret = vault_client.get_secret("secret/firstsecret").unwrap();
+    println!("{}", secret);
+    
+    Ok(())
+}
