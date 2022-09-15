e52cf9f9a10192fc884c24715a6c248b3265da02
Daniel Huckins <dhuckins@users.noreply.github.com>
2022-09-15 11:12:41 -0400

    modify script

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,19 +2,19 @@ use std::env;
 
 use hashicorp_vault;
 
-fn main() -> Result<(), ()> {
-    let vault_address = env::var("VAULT_ADDR")
-        .expect("VAULT_ADDR must be set");
-    assert!(!vault_address.is_empty());
-    
-    let vault_token = env::var("VAULT_TOKEN")
-        .expect("VAULT_TOKEN must be set");
+fn main() -> Result<(), hashicorp_vault::Error> {
+    let vault_address = env::var("VAULT_ADDR").expect("VAULT_ADDR must be set");
     assert!(!vault_address.is_empty());
 
-    let vault_client = hashicorp_vault::Client::new(vault_address, vault_token).unwrap();
+    let vault_token = env::var("VAULT_TOKEN").expect("VAULT_TOKEN must be set");
+    assert!(!vault_address.is_empty());
 
-    let secret = vault_client.get_secret("secret/firstsecret").unwrap();
-    println!("{}", secret);
-    
+    let vault_client = hashicorp_vault::Client::new(vault_address, vault_token)?;
+
+    match vault_client.get_secret("firstsecret") {
+        Ok(secret) => println!("{}", secret),
+        Err(err) => eprintln!("Error getting secret: {}", err),
+    }
+
     Ok(())
 }
