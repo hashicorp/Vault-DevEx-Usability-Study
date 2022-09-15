114e4ac32be27f770b0c410be0b8884b9eeb644b
Daniel Huckins <dhuckins@users.noreply.github.com>
2022-09-15 10:47:01 -0400

    assert not empty

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,8 +5,11 @@ use hashicorp_vault;
 fn main() -> Result<(), ()> {
     let vault_address = env::var("VAULT_ADDR")
         .expect("VAULT_ADDR must be set");
+    assert!(!vault_address.is_empty());
+    
     let vault_token = env::var("VAULT_TOKEN")
         .expect("VAULT_TOKEN must be set");
+    assert!(!vault_address.is_empty());
 
     let vault_client = hashicorp_vault::Client::new(vault_address, vault_token).unwrap();
 
