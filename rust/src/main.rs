cc1784c9edbda9080d2d8baa67bf973b703f34fa
Daniel Huckins <dhuckins@users.noreply.github.com>
2022-09-16 13:26:39 -0400

    use assume role (basically log in)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 
 use hashicorp_vault;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct MySecret {
@@ -13,21 +13,30 @@ fn main() -> Result<(), hashicorp_vault::Error> {
     let vault_address = env::var("VAULT_ADDR").expect("VAULT_ADDR must be set");
     assert!(!vault_address.is_empty());
 
-    let vault_token = env::var("VAULT_TOKEN").expect("VAULT_TOKEN must be set");
-    assert!(!vault_address.is_empty());
+    let vault_role_id = env::var("VAULT_ROLE_ID").expect("VAULT_ROLE_ID must be set");
+    assert!(!vault_role_id.is_empty());
 
-    let vault_client = hashicorp_vault::Client::new(vault_address, vault_token)?;
+    let vault_role_secret_id =
+        env::var("VAULT_ROLE_SECRET_ID").expect("VAULT_ROLE_SECRET_ID must be set");
+    assert!(!vault_role_secret_id.is_empty());
 
-    let secret: Result<MySecret, hashicorp_vault::Error> = vault_client.get_custom_secret("firstsecret");
+    let vault_client = hashicorp_vault::Client::new_app_role(
+        vault_address,
+        vault_role_id,
+        Some(vault_role_secret_id),
+    )?;
+
+    let secret: Result<MySecret, hashicorp_vault::Error> =
+        vault_client.get_custom_secret("firstsecret");
 
     match secret {
         Ok(my_secret) => {
             println!("{}", my_secret.sample_secret);
             Ok(())
-        },
+        }
         Err(err) => {
             eprintln!("error fetching secret: {}", err);
             Err(err)
-        },
+        }
     }
 }
